da06670499e79dc736dcecf1b93715d6e6a0e9f4
Jayden Hoy <jayden.hoy@student.tdsb.on.ca>
2024-12-10 11:30:41 -0500

    Setup Fire Base

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCcahKLWb99UKcP6ZXvdcetf2Mmzu--WsA",
+    authDomain: "summative-5bfb1.firebaseapp.com",
+    projectId: "summative-5bfb1",
+    storageBucket: "summative-5bfb1.firebasestorage.app",
+    messagingSenderId: "836925879910",
+    appId: "1:836925879910:web:c625ba8f8aaf3eae1d5fa7"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
